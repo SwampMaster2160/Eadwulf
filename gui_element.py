d37e7ab2885ef1b19ae693421d56e572cbeddbff
SwampMaster2160 <62127482+SwampMaster2160@users.noreply.github.com>
2021-12-28 12:35:49 +1300

    17

--- gui_element.py
+++ gui_element.py
@@ -1,4 +1,4 @@
-from gui_renderer import GUIRenderer
+from gui_renderer import GUIRenderer, GUITextureAlign
 from pixel_pos import PixelPos
 
 
@@ -11,6 +11,13 @@ class RectGUIElement:
 	pos: PixelPos
 	size: PixelPos
 	color: any
+	align: GUITextureAlign
+
+	def __init__(self, pos: PixelPos, size: PixelPos, color, align: GUITextureAlign):
+		self.pos = pos
+		self.size = size
+		self.color = color
+		self.align = align
 
 	def render(self, gui_renderer: GUIRenderer):
-		pass
+		gui_renderer.render_rect(self.color, self.pos, self.size, self.align)
--- gui_menu.py
+++ gui_menu.py
@@ -3,9 +3,11 @@ from typing import List, Tuple, Sequence
 
 import pygame as pg
 
+import gui_element
 from game_state import GameState
 from gui_element import GUIElement
-from gui_renderer import GUIRenderer
+from gui_renderer import GUIRenderer, GUITextureAlign
+from pixel_pos import PixelPos
 
 
 class GUIMenu:
@@ -19,11 +21,12 @@ class GUIMenu:
 		return GameState.IN_MENU, self
 
 	def render(self, gui_renderer: GUIRenderer):
-		pass
+		for element in self.instance_elements:
+			element.render(gui_renderer)
 
 
 class PauseGUIMenu(GUIMenu):
-	ELEMENTS = []
+	ELEMENTS = [gui_element.RectGUIElement(PixelPos(0, 0), PixelPos(100, 100), (127, 127, 127), GUITextureAlign.CENTRE_CENTRE)]
 
 	def tick(self, keys_pressed: Sequence[bool], keys_pressed_this_frame: dict) -> Tuple[GameState, any]:
 		new_game_state = GameState.IN_MENU
