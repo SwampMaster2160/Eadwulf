6b21ace71adc33e30077a0597cbc7359becca871
SwampMaster2160 <62127482+SwampMaster2160@users.noreply.github.com>
2021-12-30 18:02:55 +1300

    29

--- world.py
+++ world.py
@@ -16,6 +16,7 @@ class World:
 	player: Player = Player()
 	chunks: dict = {}
 	do_save_world: bool = 0
+	ticks_since_last_save: int = 0
 
 	def start_using_generated_chunks(self):
 		for pos, generating_chunk in self.chunks.items():
@@ -25,6 +26,10 @@ class World:
 	def tick(self, keyboard: Keyboard):
 		self.start_using_generated_chunks()
 		self.player.tick(keyboard, self)
+		self.ticks_since_last_save += 1
+		if self.ticks_since_last_save > 1000:
+			self.save()
+			self.ticks_since_last_save = 0
 
 	def render(self, world_renderer: WorldRenderer):
 		self.start_using_generated_chunks()
@@ -69,11 +74,18 @@ class World:
 		self.do_save_world = 1
 
 	def save(self):
+		save_delete_pos = self.player.pos.get_chunk_pos()
+		print("Save")
 		world_path = os.path.join(pathlib.Path.home(), "eadwulf", "world", self.filepath, "chunks")
 		os.makedirs(world_path, exist_ok=1)
+		to_del = []
 		for pos, chunk in self.chunks.items():
 			if isinstance(chunk, Chunk):
 				filename = f"{pos[0]}_{pos[1]}.ech"
 				file = open(os.path.join(world_path, filename), "wb")
 				pickle.dump(chunk, file)
 				file.close()
+				if pos[1] < save_delete_pos.y - 1 or save_delete_pos.y + 1 < pos[1]:
+					to_del.append(pos)
+		for pos in to_del:
+			del self.chunks[pos]
